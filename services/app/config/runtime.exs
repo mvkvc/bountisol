9cd244c11bb36bb461714871fee98e286e3ce98b
Marko Vukovic <mvkvc.git@gmail.com>
2023-12-06 22:09:25 -0500

    Try new runtime origin settings

--- services/app/config/runtime.exs
+++ services/app/config/runtime.exs
@@ -52,14 +52,14 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "https://app.akashi.systems"
+  host = System.get_env("PHX_HOST") || "app.akashi.systems"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :akashi, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :akashi, AkashiWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: ["https://#{host}"],
+    check_origin: ["#{host}", "https://#{host}", "//*.akashi.systems"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
