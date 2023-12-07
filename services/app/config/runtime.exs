cd133df550cc61b4891b8d4227fa98d6c885c418
Marko Vukovic <mvkvc.git@gmail.com>
2023-12-06 22:02:46 -0500

    Try changing runtime host default

--- services/app/config/runtime.exs
+++ services/app/config/runtime.exs
@@ -52,7 +52,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "app.akashi.systems"
+  host = System.get_env("PHX_HOST") || "https://app.akashi.systems"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :akashi, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
